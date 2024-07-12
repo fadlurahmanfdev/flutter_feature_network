25712f8188b835f28af10bef6ef98a9c12ae7e09
fadlurahmanf <tffajari@gmail.com>
2024-07-12 12:57:14 +0700

    add ssl feature

---
+++ example/lib/domain/interceptor/example_ssl_interceptor.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_feature_network/flutter_feature_network.dart';
+
+class ExampleSSLInterceptor extends SSLInterceptor {
+  FeatureNetworkRepository networkRepository = FeatureNetworkRepositoryImpl();
+
+  @override
+  void onHandshakeException(DioException dioException, ErrorInterceptorHandler handler) async {
+    try {
+      rootBundle.load("assets/api_bank_mas_my_id.pem").then((byteData) async {
+        print("MASUK_ GANTI DIO CLIENT");
+        final dio = networkRepository.getDio(
+          baseUrl: dioException.requestOptions.baseUrl,
+          headers: dioException.requestOptions.headers,
+          interceptors: [
+            LoggerInterceptor(),
+          ],
+          trustedCertificateBytes: byteData.buffer.asUint8List(),
+        );
+        final result = await dio.fetch(dioException.requestOptions);
+        handler.resolve(result);
+      });
+    } catch (e) {
+      handler.next(dioException);
+    }
+  }
+}
---
+++ lib/src/domain/interceptor/request_id_interceptor.dart
@@ -0,0 +1,7 @@
+// class RequestIdInterceptor extends InterceptorsWrapper {
+//   @override
+//   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+//     options.headers['X-Request-ID'] = const Uuid().v4();
+//     handler.next(options);
+//   }
+// }
