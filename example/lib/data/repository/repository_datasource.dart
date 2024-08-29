107bfee5b6ddf7518fd1a1ea5e3d730ce63c3262
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-08-29 20:17:31 +0700

    dynamic fingerprint

--- example/lib/data/repository/repository_datasource.dart
+++ example/lib/data/repository/repository_datasource.dart
@@ -10,19 +10,21 @@ abstract class RepositoryDatasource {
   Future<PostResponse> getPostByIdCorrectFingerprint({required int id});
 
   Future<PostResponse> getPostByIdIncorrectFingerprint({required int id});
+
+  Future<PostResponse> getPostByIdDynamicFingerprint({required int id});
 }
 
 class RepositoryDatasourceImpl extends RepositoryDatasource {
   Dio placeHolderStandardDio;
   Dio placeHolderCorrectFingerprintDio;
   Dio placeHolderIncorrectFingerprintDio;
-  Dio sslDio;
+  Dio placeHolderDynamicFingerprintDio;
 
   RepositoryDatasourceImpl({
     required this.placeHolderStandardDio,
     required this.placeHolderCorrectFingerprintDio,
     required this.placeHolderIncorrectFingerprintDio,
-    required this.sslDio,
+    required this.placeHolderDynamicFingerprintDio,
   });
 
   @override
@@ -61,6 +63,22 @@ class RepositoryDatasourceImpl extends RepositoryDatasource {
   Future<PostResponse> getPostByIdIncorrectFingerprint({required int id}) async {
     try {
       final res = await placeHolderIncorrectFingerprintDio.get(
+        'posts/$id',
+      );
+      final dataMap = res.data as Map<String, dynamic>? ?? {};
+      return PostResponse.fromJson(dataMap);
+    } on DioException catch (e) {
+      log("failed dio: $e");
+      throw FeatureException(title: 'Failed DIO', desc: '${e.response?.statusCode} - ${e.type} - ${e.message}');
+    } catch (e) {
+      throw FeatureException(title: 'Failed', desc: 'error: $e');
+    }
+  }
+
+  @override
+  Future<PostResponse> getPostByIdDynamicFingerprint({required int id}) async {
+    try {
+      final res = await placeHolderDynamicFingerprintDio.get(
         'posts/$id',
       );
       final dataMap = res.data as Map<String, dynamic>? ?? {};
