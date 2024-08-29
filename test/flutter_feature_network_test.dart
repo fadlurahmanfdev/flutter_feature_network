56481a0b42c185337d985b7da600f45247432beb
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-08-30 00:10:35 +0700

    update feature

--- test/flutter_feature_network_test.dart
+++ test/flutter_feature_network_test.dart
@@ -1,7 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_feature_network/flutter_feature_network.dart';
-
-void main() {
-  test('adds one to input values', () {});
-}
+void main() {}
